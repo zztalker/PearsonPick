1406c8cbcef86648eb5662f2b1ccc60d23215eea
Pavel Zaikin <zaikinpv@gmail.com>
2015-10-24 09:19:49 +0500

    Main update
    Added some additional functions and get data from file and save report
    to file.

--- pearson.py
+++ pearson.py
@@ -41,18 +41,8 @@ class fI:
         s += "\n    y0: "+str(self.y0)
         s += "\n"
         return s
-    def fun(self,x):		
+    def fun(self,x):        
         return self.y0*pow(1+x/self.a[1],self.m[1])*pow(1-x/self.a[2],self.m[2])
-
-class fIplot(fI):
-    def __init__(self,y0,a1,a2,m1,m2):
-        self.m = ["m coefficient",0.,0.]
-        self.a = ["a coefficient",0.,0.]
-        self.a[1]=a1
-        self.a[2]=a2
-        self.m[1]=m1
-        self.m[2]=m2
-        self.y0=y0
 
 
 class fII:
@@ -70,64 +60,114 @@ class fII:
     def fun(self,x):
         return self.y0*pow(1-x*x/(self.a*self.a),self.m)
 
-class fIIplot(fII):
-    def __init__(self,y0,a,m):
-        self.a=a
-        self.m=m
-        self.y0=y0
+class fIII:
+    def __init__(self, r, sigma, summ):
+        self.p = 4 / pow(r[3], 2) - 1
+        self.l = ['granica l', 0., 0.]
+        self.l[2] = sigma * (2 / r[3] - r[3] / 2)
+        self.l[1] = -self.l[2]
+        self.n0 = (summ / self.l[2]) * ((self.p + 1) / (pow(math.e, self.p) * sp.gamma(self.p + 1)))
+
+        return
+    def __str__(self):
+        s = """ Type III function params:
+         l : {0}
+         p : {1}
+         n0 : {2}""".format(self.l,self.p,self.n0)
+        return s
+    def fun(self, x):
+        return self.n0 * pow(1 + x / self.l[2], self.p) * pow(math.e, -self.p * x / self.l[2])
+
 
 
 class fIV:
-    def __init__(self,beta,r,mu,M):
-        #посчитаем необходимые коэффициенты
-        self.a = []
-        self.m = []
-        self.y0 = 0
+    def __init__(self, r, sigma, summ):
+        R = (6 * (r[4] - r[3] ** 2 - 1)) / (2 * r[4] - 3 * r[3] ** 2 - 6)
+        print(R)
+        self.q = (R + 2) / 2
+        self.v = -(R * (R - 2) * r[3]) / math.sqrt(16 * (R - 1) - pow(r[3], 2) * pow(R - 2, 2))
+        print('q=', self.q)
+        print('v=', self.v)
+        # print('sqrt=', 16 * (R - 1) - pow(r[3], 2) * pow(R - 2, 2))
+        self.l = ['granica l', 0., 0.]
+        self.l[2] = (sigma / 4) * math.sqrt(16 * (R - 1) - pow(r[3], 2) * pow(R - 2, 2))
+        self.l[1] = -self.l[2]
+        # print(R, ' ', self.v)
+        self.n0 = (summ / self.l[2]) * (1 / 1.80753)
         return
-        sigma = math.sqrt(mu[2])
-        p = np.poly1d([1,-(r-2),-(r-1)+(4*r*r*(r+1))/(beta[1]*(r+2)*(r+2)+16*(r+1))])
-        m = p.r
-        self.m = [1,0.,0.]
-        if mu[3]>0:
-            self.m[2] = max(m)
-            self.m[1] = min(m)
-        else:
-            self.m[1] = max(m)
-            self.m[2] = min(m)
-        print( beta[1]*(r+2)*(r+2)+16*(r+1))
-        v = sigma/(2*(r-2))*math.sqrt(beta[1]*(r+2)*(r+2)+16*(r+1))
-        self.a = [1,0.,0.]
-        self.a[1] = v*self.m[1]-M
-        self.a[2] = v*self.m[2]+M
+
+    def fun(self, x):
+        return self.n0 * pow(1 + x ** 2 / self.l[2] ** 2, -self.q) * pow(math.e, -self.v * math.atan(x / self.l[2]))
+
+ #   def __init__(self,beta,r,mu,M):
+ #        #посчитаем необходимые коэффициенты
+ #        self.a = []
+ #        self.m = []
+ #        self.y0 = 0
+ #        sigma = math.sqrt(mu[2])
+ #        p = np.poly1d([1,-(r-2),-(r-1)+(4*r*r*(r+1))/(beta[1]*(r+2)*(r+2)+16*(r+1))])
+ #        m = p.r
+ #        self.m = [1,0.,0.]
+ #        if mu[3]>0:
+ #            self.m[2] = max(m)
+ #            self.m[1] = min(m)
+ #        else:
+ #            self.m[1] = max(m)
+ #            self.m[2] = min(m)
+ #        print(beta[1]*(r+2)*(r+2)+16*(r+1))
+ #        v = sigma/(2*(r-2))*math.sqrt(beta[1]*(r+2)*(r+2)+16*(r+1))
+ #        self.a = [1,0.,0.]
+ #        self.a[1] = v*self.m[1]-M
+ #        self.a[2] = v*self.m[2]+M
+  #       
+   #      y0_1 = pow(self.a[1],self.m[1])*pow(self.a[2],self.m[2])
+ #        y0_1 = y0_1/pow(self.a[1]+self.a[2],self.m[1]+self.m[2]+1)
+  #       
+  #       y0_2 = sp.gamma(self.m[1]+self.m[2]+2)/(sp.gamma(self.m[1]+1)*sp.gamma(self.m[2]+1))
         
-        y0_1 = pow(self.a[1],self.m[1])*pow(self.a[2],self.m[2])
-        y0_1 = y0_1/pow(self.a[1]+self.a[2],self.m[1]+self.m[2]+1)
-        
-        y0_2 = sp.gamma(self.m[1]+self.m[2]+2)/(sp.gamma(self.m[1]+1)*sp.gamma(self.m[2]+1))
-        
-        self.y0 = y0_1*y0_2        
-        return
+ #        self.y0 = y0_1*y0_2        
+  #       return
     def __str__(self):
-        s = "Type I function params:\n"
-        s += "\n    a1,a2: "+str(self.a)
-        s += "\n    m1,m2: "+str(self.m)
-        s += "\n    y0: "+str(self.y0)
+        s = "Type VI function params:\n"
+        s += "\n    l: "+str(self.l)
+        s += "\n    q: "+str(self.q)
+        s += "\n    v: "+str(self.v)
         s += "\n"
         return s
-    def fun(self,x):		
-        return self.y0*pow(1+x/self.a[1],self.m[1])*pow(1-x/self.a[2],self.m[2])
+   #  def fun(self,x):        
+    #     return self.y0*pow(1+x/self.a[1],self.m[1])*pow(1-x/self.a[2],self.m[2])
     
+
+
+class fV:
+    def __init__(self, r, sigma, summ):
+        self.l = ['granica l', 0., 2.5]
+        self.p = 4 + math.sqrt((8 + 4 * math.sqrt(4 + r[3] ** 2)) / r[3] ** 2)
+        self.v = sigma * (self.p - 2) * math.sqrt(self.p - 3)
+        self.n0 = (summ * pow(self.v, self.p - 1)) / (sp.gamma(self.p - 1))
+        return
+
+    def __str__(self):
+        s = ""
+        return s
+    def fun(self, x):
+        return self.n0 * pow(x, - self.p) * pow(math.e, - self.v / x)
+
 
 class pearson:
     def __init__(self,x,p):
         mu = self.moments(x,p)
-        self.sigma = math.sqrt(mu[2])		
+        self.sigma = math.sqrt(mu[2])       
         self.beta = ["beta array",0.,0.]
         self.beta[1] = pow(mu[3],2)/pow(mu[2],3)
         self.beta[2] = mu[4]/pow(mu[2],2)
-        beta = self.beta		
+        beta = self.beta        
         self.r = 6.*(beta[2]-beta[1]-1.)/(3.*beta[1]-2.*beta[2]+6.)
         r = self.r
+        self.rk = ['r-koef', 0., 0., 0., 0.]
+        self.rk[3] = mu[3] / pow(self.sigma, 3)
+        self.rk[4] = mu[4] / pow(self.sigma, 4)
+
         self.b = [0.,0.,0.]
         b = self.b
         b[0] = mu[2]*(r+1.)/(r-2.)
@@ -135,47 +175,88 @@ class pearson:
         b[2] = -1./(r-2.)
         self.M = -b[1]
         self.k = pow(b[1],2)/(4*b[2]*b[0])
-        if self.k<0:
+        if round(self.k,1)==0:
+            self.type = "II"
+            self.f = fII(beta,mu)
+        elif round(self.k,0)==1:
+            self.type = "V"
+            self.f = fV(self.rk,self.sigma,self.Mx)
+        elif (self.k<-1):
+            self.f = fIII(self.rk,self.sigma,self.Mx)
+            self.type = "III"
+        elif self.k<0:
             self.type = "I"
             self.f = fI(beta,r,mu,self.M)
-        elif (self.k==0):
-            self.type = "?"
-            self.f = func_pearson()
         elif (self.k<1):
             self.type = "IV"
-            self.f = fIV(beta,r,mu,self.M)
-        elif (self.k==1):
-            self.f = func_pearson()
-            self.type = "?"
+            self.f = fIV(self.rk,self.sigma,self.Mx)
         elif (self.k>1):
-            self.f = func_pearson()
-            self.type = "?"
+            self.f = fIII(self.rk,self.sigma,self.Mx)
+            self.type = "III"
         else:
             self.f = func_pearson()
             self.type = "unknown! "+str(self.k)
     def __str__(self):
         s = "Pearson params: \n"
-        s += "\n	Mx: "+str(self.Mx)
-        s += "\n	central moments: "+str(self.mu)
-        s += "\n	sigma: "+str(self.sigma)
-        s += "\n	beta[1,2]: "+str(self.beta[1:])
-        s += "\n	r: "+str(self.r)
-        s += "\n	b[0,1,2]: "+str(self.b)
-        s += "\n	M: "+str(self.M)
-        s += "\n	k: "+str(self.k)
-        s += "\n    type of curve:"+str(self.type)
-        s += "\n\n  Curve params:"
+        s += "\nXmin: "+str(self.Xmin)  
+        s += "\nXmax: "+str(self.Xmax)
+        s += "\nc: "+str(self.c)
+        s += "\nMx: "+str(self.Mx)
+        s += "\nXa: "+str(self.Xa)
+        s += "\nmoments: "
+        s += "\nN\tnormal\tcentral"
+        for i in range(1,5):
+            s += "\n{0}\t{1}\t{2}".format(i,self.m[i],self.mu[i])
+        s += "\n\nsigma: "+str(self.sigma)
+        s += "\nbeta[1,2]: "+str(self.beta[1:])
+        s += "\nr: "+str(self.r)
+        s += "\nb[0,1,2]: "+str(self.b)
+        s += "\nM: "+str(self.M)
+        s += "\nk: "+str(self.k)
+        s += "\ntype of curve:"+str(self.type)
+        s += "\n\nCurve params:"
         s += str(self.f)
         s += "\n"
         return s
     def moments(self,x,p):
+        self.m = ["momenth array",0,0,0,0]
         self.mu = ["central momenth array",0,0,0,0]
         self.Mx = 0
+        self.Xmax = x[0]
+        self.Xmin = x[0]
         for i in range(len(x)):
+            if self.Xmax < x[i]: self.Xmax = x[i]
+            if self.Xmin > x[i]: self.Xmin = x[i]            
             self.Mx += p[i]*x[i]
+        self.c = (self.Xmax-self.Xmin)/(x.shape[0]-1)
+        #Get an Xa - there is X from middle of array
+        tSum = 0
         for i in range(len(x)):
-            self.mu[1] += p[i]*((x[i]-self.Mx))
-            self.mu[2] += p[i]*pow(x[i]-self.Mx,2)
-            self.mu[3] += p[i]*pow(x[i]-self.Mx,3)
-            self.mu[4] += p[i]*pow(x[i]-self.Mx,4)
+            tSum += p[i]
+            if tSum >= 0.5:
+                # Get a middle! 
+                self.Xa = x[i]+self.c/2
+                break
+        
+        self.x_ = x.copy()
+        self.x__ = x.copy()
+    
+        for i in range(len(x)):
+            self.x_[i] = x[i]+self.c-self.Xa
+            self.x__[i] = (x[i]+self.c-self.Xa)/self.c
+            self.m[1] += p[i]*(((x[i]+self.c/2)-self.Xa)/self.c)
+            self.m[2] += p[i]*pow(((x[i]+self.c/2)-self.Xa)/self.c,2)
+            self.m[3] += p[i]*pow(((x[i]+self.c/2)-self.Xa)/self.c,3)
+            self.m[4] += p[i]*pow(((x[i]+self.c/2)-self.Xa)/self.c,4)
+
+        self.mu[1] = 0
+        self.mu[2] = self.m[2] - self.m[1] ** 2
+        self.mu[3] = self.m[3] - 3 * self.m[2] * self.m[1] + 2 * pow(self.m[1], 3)
+        self.mu[4] = self.m[4] - 4 * self.m[3] * self.m[1] + 6 * self.m[2] * pow(self.m[1], 2) - 3 * pow(self.m[1], 4)
+
+#        for i in range(len(x)):
+#            self.mu[1] += p[i]*((x[i]-self.Mx))
+#            self.mu[2] += p[i]*pow(x[i]-self.Mx,2)
+#            self.mu[3] += p[i]*pow(x[i]-self.Mx,3)
+#            self.mu[4] += p[i]*pow(x[i]-self.Mx,4)
         return self.mu
--- pickup_pearson.py
+++ pickup_pearson.py
@@ -12,27 +12,12 @@ rc('text.latex',preamble='sepackage[utf8]{inputenc}')
 rc('text.latex',preamble='sepackage[russian]{babel}')
 
 
-#x = np.arange(1.5,21,1)
-#y = np.array([0.11,0.69,3.67,4.93,2.23,1.1,1.8,2.62,4.69,2.41,1.14,7.26,9.34,5.70,8.15,12.25,19.64,8.42,3.79,0.51]);
-
-#x = np.array([20,25,30,35,40,45,50,55,60,65,70,75,80],'float')
-#x = x + 2.5
-#y = np.array([11.,93.,162.,178.,176.,132.,101.,67.,40.,24.,12.,3.,1.])
+data_array = 6
 
 
-#x = np.array([-0.5,	63.7,	76.09,	86.58,	101.47,	122.12,	219.2,	248.24],'float')
-#y = np.array([0.5,	1	,2.5	,3.5,	10,	25,	30,	27.5])
+x,y = np.loadtxt("data{0}.csv".format(data_array),delimiter=",")
 
-#x = np.array([0.5,	13.7,	36.09,	56.58,	61.47,	80.12,	90.2,	108.24],'float')
-#y = np.array([30,	20,	10,	16,	5,	4,	3,	2])
-
-x = np.array([0,	1,	2,	3,	4,	5,	6,	7, 	8,	 9],'float')
-y = np.array([29.5,	15,	12,	11,	10,	6.5,	6,	5,	4,	3],'float')
-
-
-#y = y/100
-#x = np.array([3,4,5,6,7,8],'float')
-#y = np.array([2,8,20,40,20,10],'float')
+print("Test data")
 p = y / y.sum()
 p_cal = p.copy()
 
@@ -53,39 +38,70 @@ if x.shape != y.shape:
 # p - массив вероятностей
 
 pears = pearson.pearson(x,p)
+
+print("X","N(p)","X-Xa","(X-Xa)/c",sep="\t")
+for i in range(0,x.shape[0]):
+    print(x[i],y[i],pears.x_[i],pears.x__[i],sep="\t")
+
 print(pears)
 
-x1 = np.linspace(-pears.f.a[1],pears.f.a[2],100)
+#Calc function points
+lB = np.min(pears.x__)+5
+rB = np.max(pears.x__)+5
+x1 = np.linspace(lB,rB,1000)
 y1 = np.ndarray(x1.shape)
-for i in range(0,100):
+for i in range(0,1000):
     y1[i] = pears.f.fun(x1[i])
 
-leftBorder = -pears.f.a[1]
-ii = 0
-for i in x:
-    rightBorder = i-pears.Mx
-    p_cal[ii] = spint.quad(pears.f.fun,leftBorder,rightBorder)[0]
-    ii+=1
-    leftBorder = rightBorder
 
-#plt.subplot(121)
-plt.bar(x,p, width=20.0, color="red")
-plt.bar(x,p_cal,width=10., color="green")
-plt.ylabel("% состава смеси")
-plt.xlabel("T (C) температура кипения")
-plt.show()
+#Claculation for BAR - there is an ERROR
+#leftBorder = -pears.f.a[1]
+#ii = 0
+#for i in x:
+#    rightBorder = i-pears.Mx
+#    p_cal[ii] = spint.quad(pears.f.fun,leftBorder,rightBorder)[0]
+#    ii+=1
+#    leftBorder = rightBorder
 
-plt.bar(x,p, width=20.0, color="red")
-#plt.bar(x,p_cal,width=10., color="green")
-plt.ylabel("% состава смеси")
-plt.xlabel("T (C) температура кипения")
-plt.show()
+#Original Datas
+plt.bar(pears.x__,y,  color="white") #width=pears.c,
+plt.ylabel("N")
+plt.xlabel(r"$x = (X - X_a)/c$")
+plt.title(r'Histogram of $x$')
+plt.savefig('data_bar_%d.png'%data_array)
+plt.clf()
 
-#plt.subplot(122)
+#Curve
 plt.plot(x1,y1)
 plt.ylabel("P")
-plt.xlabel("T (C) температура кипения")
+plt.xlabel("X")
+plt.title('Pearson-Curve TYPE: {0}'.format(pears.type))
+plt.savefig('data_curve_%d.png'%data_array)
+plt.clf()
+
+#Curve and Bar
+plt.bar(pears.x__,p,  color="white") #width=pears.c,
+plt.plot(x1,y1)
+plt.ylabel("P")
+plt.xlabel("X")
+plt.title('Compare Bar and Pearson-Curve TYPE: {0}'.format(pears.type))
+plt.savefig('data_curve_bar_%d.png'%data_array)
+
+
+exit()
+
+#Compare bars
+#plt.subplot(121)
+plt.bar(x,p, width=pears.c, color="white")
+plt.bar(x,p_cal,width=pears.c/2, color="lightgrey")
+plt.ylabel("P")
+plt.xlabel("X")
+plt.title(r"Histogram of $x$ and calculated $x'$")
 plt.show()
+exit()
+
+
+#plt.subplot(122)
 
 # посчитаем квантиль распределения хи квадрат
 chi2 = 0.0
